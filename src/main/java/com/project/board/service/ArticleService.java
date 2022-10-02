dda7ac74ee94f67754c79045475b4c165860f58d
eunhyeok <ceh2805@gmail.com>
2022-10-03 00:31:47 +0900

    #29 - 해시태그 검색 페이지를 위한기능 구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를 정리해서 보여주는 기능 넣기 위해
    해시태그만 뽑아서 unique하게 보여주는 쿼리를 querydsl로 작성.

--- src/main/java/com/project/board/service/ArticleService.java
+++ src/main/java/com/project/board/service/ArticleService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
@@ -63,4 +64,21 @@ public class ArticleService {
         articleRepository.deleteById(articleId);
     }
 
+    public long getArticleCount() {
+        return articleRepository.count();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticlesViaHashtag(String hashtag, Pageable pageable) {
+        if(hashtag == null || hashtag.isBlank())
+        {
+            return Page.empty(pageable);
+        }
+
+        return articleRepository.findByHashtag(hashtag, pageable).map(ArticleDto::from);
+    }
+
+    public List<String> getHashtags() {
+        return articleRepository.findAllDistinctHashtags();
+    }
 }
