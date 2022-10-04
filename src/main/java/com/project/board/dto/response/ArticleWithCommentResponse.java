acbd964c1e48753c008f4c38aedf2f5afe96148f
eunhyeok <ceh2805@gmail.com>
2022-10-04 19:51:10 +0900

    #36 - dto들에 Serializable 제거

--- src/main/java/com/project/board/dto/response/ArticleWithCommentResponse.java
+++ src/main/java/com/project/board/dto/response/ArticleWithCommentResponse.java
@@ -2,7 +2,6 @@ package com.project.board.dto.response;
 
 import com.project.board.dto.ArticleWithCommentsDto;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -17,7 +16,7 @@ public record ArticleWithCommentResponse(
         String email,
         String nickname,
         Set<ArticleCommentResponse> articleCommentResponses
-) implements Serializable {
+) {
 
     public static ArticleWithCommentResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
         return new ArticleWithCommentResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
